64196288358d2b4f4377f6ce19b57d23c27d48df
Patrik <rambo3swe@gmail.com>
2022-12-01 06:28:28 +0100

    day1 done

--- day1/main.go
+++ day1/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -21,18 +23,77 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	inputs := []int{}
+	inputs := []string{}
 
 	for scanner.Scan() {
-		i1, err := strconv.Atoi(scanner.Text())
-		if err != nil {
-			log.Fatal(err)
-		}
-		inputs = append(inputs, i1)
+		inputs = append(inputs, scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+	elfs := ParseElfs(inputs)
 
+	most := GetMostCalories(elfs)
+
+	top3 := Top3Calories(elfs)
+
+	fmt.Printf("Elf carrying the most %d and top3 %d", most, top3)
+}
+
+func ParseElfs(input []string) []Elf {
+	elfs := []Elf{}
+	e := Elf{}
+	for _, in := range input {
+
+		iIn, err := strconv.Atoi(in)
+
+		if err != nil {
+			elfs = append(elfs, e)
+			e = Elf{}
+		}
+
+		e.Items = append(e.Items, iIn)
+	}
+
+	elfs = append(elfs, e)
+
+	return elfs
+}
+
+func GetMostCalories(elfs []Elf) int {
+	most := 0
+	for _, e := range elfs {
+		if e.Total() > most {
+			most = e.Total()
+		}
+	}
+
+	return most
+}
+
+func Top3Calories(elfs []Elf) int {
+	allElfTotal := []int{}
+
+	for _, e := range elfs {
+		allElfTotal = append(allElfTotal, e.Total())
+	}
+
+	sort.Slice(allElfTotal, func(i, j int) bool {
+		return allElfTotal[i] > allElfTotal[j]
+	})
+	return allElfTotal[0] + allElfTotal[1] + allElfTotal[2]
+}
+
+type Elf struct {
+	Items []int
+}
+
+func (e *Elf) Total() int {
+	total := 0
+
+	for _, item := range e.Items {
+		total += item
+	}
+	return total
 }
--- day1/main_test.go
+++ day1/main_test.go
@@ -1,7 +1,21 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
-func TestAbc(t *testing.T) {
+func TestElfs(t *testing.T) {
+	input := []string{"1000", "2000", "3000", "", "4000", "", "5000", "6000", "", "7000", "8000", "9000", "", "10000"}
+
+	elfs := ParseElfs(input)
+
+	most := GetMostCalories(elfs)
+
+	assert.Equal(t, 24000, most)
+
+	top3 := Top3Calories(elfs)
+
+	assert.Equal(t, 45000, top3)
 
 }
